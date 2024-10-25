a87cd6fa1635f7580b513e96b70330476bb5da55
Zcrafting34 <cristian.gon.bek@gmail.com>
2024-10-24 21:08:52 -0700

    first-commi

---
+++ src/test/java/com/atomprogrammers/crud_java/CrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomprogrammers.crud_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
